acdba4ab8a2e73e80dce7a2fbf207809dffde003
DmitryHudrich <dimaprudnikov9@gmail.com>
2023-06-08 01:33:07 +0300

    Добавьте файлы проекта.

---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    internal class Player
+    {
+        private readonly string _folderPath;
+        private readonly string _files;
+
+        public Player(string folderPath)
+        {
+            if (!string.IsNullOrEmpty(folderPath))
+            {
+                _folderPath = folderPath;
+
+
+            }
+
+        }
+        public void Play()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(_folderPath);
+            FileInfo[] files = dirInfo.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                using (var audioPlayer = new WaveOutEvent())
+                {
+                    // Загрузка аудиофайла
+                    using (var audioFileReader = new AudioFileReader(file.FullName))
+                    {
+                        audioPlayer.Init(audioFileReader);
+
+                        audioPlayer.Play();
+
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            Console.Write("Папка с музыкой: ");
+            string musicPath = Console.ReadLine();
+
+            Player player = new(musicPath);
+            player.Play();
+        }
+    }
+}
